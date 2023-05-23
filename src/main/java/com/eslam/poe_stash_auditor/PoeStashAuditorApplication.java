6e87e862f35da64bb5e235864c0b6d813d8f43cd
Eslam Allam <86079760+eslam-allam@users.noreply.github.com>
2023-05-23 10:26:27 +0300

    feat: Added initial spring template

---
+++ src/main/java/com/eslam/poe_stash_auditor/PoeStashAuditorApplication.java
@@ -0,0 +1,13 @@
+package com.eslam.poe_stash_auditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoeStashAuditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoeStashAuditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eslam/poe_stash_auditor/PoeStashAuditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eslam.poe_stash_auditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoeStashAuditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
