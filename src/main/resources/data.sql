e53a4df90a4621d92f27915c7bbbac696e879c21
Eslam Allam <86079760+eslam-allam@users.noreply.github.com>
2023-06-03 20:15:58 +0300

    fix: psql data init command

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,11 @@
-INSERT IGNORE INTO user_role(id, role_code, description) VALUES (1 ,'SUPER_MAIN_ADMIN', 'Super user used by this application to access services');
-INSERT IGNORE INTO user_role(id, role_code, description) VALUES (2 ,'ADMIN', 'Admin User with global access');
-INSERT IGNORE INTO user_role(id, role_code, description) VALUES (3 ,'BASE_USER', 'Base user with minimum permissions');
+-- INSERT IGNORE INTO user_role(id, role_code, description) VALUES (1 ,'SUPER_MAIN_ADMIN', 'Super user used by this application to access services');
+-- INSERT IGNORE INTO user_role(id, role_code, description) VALUES (2 ,'ADMIN', 'Admin User with global access');
+-- INSERT IGNORE INTO user_role(id, role_code, description) VALUES (3 ,'BASE_USER', 'Base user with minimum permissions');
+INSERT INTO user_role(id, role_code, description) VALUES 
+(1 ,'SUPER_MAIN_ADMIN', 'Super user used by this application to access services') ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO user_role(id, role_code, description) VALUES (2 ,'ADMIN', 'Admin User with global access')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO user_role(id, role_code, description) VALUES (3 ,'BASE_USER', 'Base user with minimum permissions')
+ON CONFLICT (id) DO NOTHING;
